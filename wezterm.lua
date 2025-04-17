41d569b9255941ae7bb78370c66911383e0b6e71
Kevin Pankonen <kevin.p@teachingstrategies.com>
2025-04-17 15:47:24 -0700

    change behavior when clicking on url

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local mux = wezterm.mux
 local act = wezterm.action
@@ -24,12 +24,13 @@ local config = {
   quit_when_all_windows_are_closed = false,
   use_dead_keys = false,
   audible_bell = "Disabled",
-  default_prog = { shell, '-l' },
+  default_prog = { shell, "-l" },
   check_for_updates = false,
 
   initial_rows = 30,
   initial_cols = 100,
   scrollback_lines = 1000000,
+  native_macos_fullscreen_mode = false,
 
   adjust_window_size_when_changing_font_size = false,
   hide_tab_bar_if_only_one_tab = true,
@@ -37,27 +38,45 @@ local config = {
 
   selection_word_boundary = " \t\n{}[]()\"'`$|:*",
 
-  color_scheme = 'Tango (terminal.sexy)',
+  color_scheme = "Tango (terminal.sexy)",
 
   colors = {
-    foreground = 'white',
-    cursor_bg = 'white',
-    cursor_fg = 'black',
-    selection_fg = 'black',
-    selection_bg = 'white',
-    split = '#c9c5c5',
+    foreground = "white",
+    cursor_bg = "white",
+    cursor_fg = "black",
+    selection_fg = "black",
+    selection_bg = "white",
+    split = "#c9c5c5",
   },
 
-  font = wezterm.font('Cousine Nerd Font', { weight = 'Medium' }),
+  font = wezterm.font("Cousine Nerd Font", { weight = "Medium" }),
   font_size = font_size,
   warn_about_missing_glyphs = false,
 
   keys = {
-    { key = 'd', mods = alt_key, action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
-    { key = 'D', mods = alt_key, action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
-    { key = "[", mods = alt_key, action = act.ActivatePaneDirection("Prev")},
-    { key = "]", mods = alt_key, action = act.ActivatePaneDirection("Next")},
-  }
+    { key = "[", mods = alt_key, action = act.ActivatePaneDirection("Prev") },
+    { key = "]", mods = alt_key, action = act.ActivatePaneDirection("Next") },
+    { key = "D", mods = alt_key, action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "d", mods = alt_key, action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "l", mods = "SHIFT|CTRL", action = act.ClearScrollback("ScrollbackOnly") },
+  },
+  mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "NONE",
+      action = wezterm.action.CompleteSelection("ClipboardAndPrimarySelection"),
+    },
+    {
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = alt_key,
+      action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+    {
+      event = { Down = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = wezterm.action.Nop,
+    },
+  },
 }
 
 if is_darwin then
@@ -74,11 +93,11 @@ else
     })
   end
 
-  table.insert(config.keys, { key = 'v', mods = alt_key, action = act.PasteFrom 'Clipboard' })
-  table.insert(config.keys, { key = 'v', mods = 'SUPER', action = act.PasteFrom 'Clipboard' })
-  table.insert(config.keys, { key = 'c', mods = alt_key, action = act.CopyTo 'ClipboardAndPrimarySelection' })
-  table.insert(config.keys, { key = 'c', mods = 'SUPER', action = act.CopyTo 'ClipboardAndPrimarySelection' })
-  table.insert(config.keys, { key = 't', mods = alt_key, action = act.SpawnTab 'CurrentPaneDomain' })
+  table.insert(config.keys, { key = "v", mods = alt_key, action = act.PasteFrom("Clipboard") })
+  table.insert(config.keys, { key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") })
+  table.insert(config.keys, { key = "c", mods = alt_key, action = act.CopyTo("ClipboardAndPrimarySelection") })
+  table.insert(config.keys, { key = "c", mods = "SUPER", action = act.CopyTo("ClipboardAndPrimarySelection") })
+  table.insert(config.keys, { key = "t", mods = alt_key, action = act.SpawnTab("CurrentPaneDomain") })
 end
 
 return config
